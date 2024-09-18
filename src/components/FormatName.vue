ce0a7a8ca1fdde224b6d2ec5d8e7d72e1eb4c811
Doan01062002 <vandoan01062002@gmail.com>
2024-09-18 16:46:37 +0700

    session02

---
+++ src/components/FormatName.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Họ và tên: {{ name }}</h1>
+  </div>
+</template>
+
+<script setup>
+const user = {
+  firstName: "Nguyễn Văn",
+  lastName: "Đoan",
+};
+
+function forMatName(user) {
+  const name = user.firstName + " " + user.lastName;
+  return name;
+}
+
+const name = forMatName(user);
+</script>
+
+<style scoped></style>
